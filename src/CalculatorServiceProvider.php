f42ac5362731b0ff616ece444dff53664e40374c
TonyKang6743 <langkang0809@gmail.com>
2023-11-22 15:25:27 +0800

    update

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Nmslol\Calculator;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('calculator', function ($app) {
+            return new Calculator();
+        });
+    }
+}
